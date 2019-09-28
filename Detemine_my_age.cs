4a5927e392ac054dd9067696fc85ae6c0270c2d8
Eng.Abdulwahab <39528190+Hjd959@users.noreply.github.com>
2019-09-28 16:34:00 +0300

    Update Detemine_my_age.cs

--- Detemine_my_age.cs
+++ Detemine_my_age.cs
@@ -10,6 +10,15 @@ namespace Detemine_my_age
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Welcom to Deteine yout age");
+
+            Console.WriteLine("Enter Your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello" + " " + name);
+          //  Console.ReadLine();
+
+
+            Console.WriteLine("Enter your age // Like -> YYYY-MM-DD");
             // n حساب العمر
             // Determin my age
 
@@ -17,7 +26,8 @@ namespace Detemine_my_age
 
             // n البيانات المدخله نوع سترنق وانا ابيها داتا تايم لذالك لازم احولها الى نوع داتا تايب
             // Type ReadLine is string and me whent DataTime .. Must be transformation to DataTime
-            MyAge =Convert.ToDateTime(Console.ReadLine());
+
+            MyAge = Convert.ToDateTime(Console.ReadLine());
 
 
 
@@ -31,9 +41,9 @@ namespace Detemine_my_age
 
             // n بعد ذالك لازم نحولها
             // after that must be convert 
-            int year =Convert.ToInt32(DateTime.Now.Subtract(MyAge).TotalDays / 360);
-            Console.WriteLine("YYYY-MM-DD");
-            Console.WriteLine("Your age is : ");
+            int year = Convert.ToInt32(DateTime.Now.Subtract(MyAge).TotalDays / 360);
+            
+            Console.WriteLine(name + " " + "Your age is : " + year);
 
         }
     }
